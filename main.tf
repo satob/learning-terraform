9daca06f0a7b8d599fa985cff54937c3ad85eced
SATO Yusuke <yusuke.sato.zz@gmail.com>
2024-02-23 16:55:31 +0900

    Fix typo

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = [va.ami_filter.name]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [va.ami_filter.owner]
+  owners = [var.ami_filter.owner]
 }
 
 data "aws_vpc" "default" {
